e6979c3941eda69a128fd4550f32ae4b27117629
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2021-12-31 23:10:53 -0500

    still in progress

---
+++ main.py
@@ -0,0 +1,15 @@
+mystr = "TRUE"
+for true in mystr:
+    print ('TRUE')
+    continue
+
+numbers = [2, 3, 10]
+for numbers in range(2, 10):
+   print(numbers)
+
+
+chore_list = ["Laundry", "mop floors", "clean bathroom", "clean fridge", "vacuum", "dusting", "cooking", "grocery shop", "car wash" , "pay bills"]
+chore_list.remove("pay bills")
+print(chore_list)
+print(True)
+
